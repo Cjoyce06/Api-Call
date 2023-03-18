f5c458f1110533b55f6977ad7c74f3b45535430b
Colin Joyce <Cjoyce1@bsd220.org>
2023-03-18 18:35:16 -0500

    Initial Commit

---
+++ Api Call/Api_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_CallApp.swift
+//  Api Call
+//
+//  Created by Colin Joyce on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Api_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Api Call/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Api Call
+//
+//  Created by Colin Joyce on 3/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
